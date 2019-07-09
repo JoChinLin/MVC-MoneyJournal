ac7ad9f5cc2099ee6ee386bc984b9ee1bf72effb
ljc <ljc@tunghosteel.com>
2019-07-09 10:19:51 +0800

    add partialview modified home controller,index view

--- MoneyJournal/Controllers/HomeController.cs
+++ MoneyJournal/Controllers/HomeController.cs
@@ -1,18 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using MoneyJournal.Models.ViewModels;
 
 namespace MoneyJournal.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private List<MoneyUsage> dataInit = null;
+        public HomeController()
         {
-            return View();
+
+            if (dataInit == null)
+            {
+                dataInit = this.GetMoneyUsageData();
+            }
         }
 
+        public ActionResult Index()
+        {
+            var dataQuery = dataInit.AsQueryable();
+
+            var dataList = dataQuery.OrderBy(oo => oo.CostDate).Select(ss => new MoneyUsage()
+            {
+                CostType = ss.CostType,
+                CostDate = ss.CostDate,
+                Amount = ss.Amount
+            }).ToList();
+
+            return View(dataList);
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -26,5 +44,78 @@ namespace MoneyJournal.Controllers
 
             return View();
         }
+
+
+        private List<MoneyUsage> GetMoneyUsageData()
+        {
+
+
+            var types = new string[] { "支出", "收入" };
+
+            var resultList = new List<MoneyUsage>();
+
+            for (var i = 1; i < 101; i++)
+            {
+                var randomGen = new RandomGenerator();
+
+                var data = new MoneyUsage();
+
+                data.CostType = types[randomGen.RandomCostType(i)];
+
+                data.CostDate = randomGen.RandomCostDate(i);
+
+                data.Amount = randomGen.RandomAmount(i);
+
+                resultList.Add(data);
+            }
+
+            return resultList;
+        }
+
+
     }
+    internal class RandomGenerator
+    {
+        private static readonly Random random = new Random();
+        private static readonly object syncLock = new object();
+        public int RandomAmount(int num)
+        {
+            lock (syncLock)
+                return random.Next(100, 30000);
+
+        }
+
+        public DateTime RandomCostDate(int num)
+        {
+            DateTime start = new DateTime(2018, 1, 1);
+            var range = (DateTime.Today - start).Days;
+
+            var max = 0;
+            var min = 0;
+            if (range > num)
+            {
+                max = range;
+                min = num;
+            }
+            else
+            {
+                max = num;
+                min = range;
+            }
+            var date = start.AddDays(random.Next(min, max));
+
+
+            lock (syncLock)
+                return date;
+
+        }
+        public int RandomCostType(int num)
+        {
+            lock (syncLock)
+                return random.Next(0, 2);
+
+        }
+    }
+
+
 }
---
+++ MoneyJournal/Models/ViewModels/MoneyUsage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MoneyJournal.Models.ViewModels
+{
+    public class MoneyUsage
+    {
+        public string CostType { get; set; }
+
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+        public DateTime CostDate { get; set; }
+
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:N0}")]
+        public int Amount { get; set; }
+
+        public string Remark { get; set; }
+    }
+}
